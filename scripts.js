ac06450f9538a6a3484b2d2997c7f007c9729ba1
Walison Guinzani <w.guinzani@gmail.com>
2021-07-14 22:39:26 -0300

    add-string

---
+++ scripts.js
@@ -0,0 +1,26 @@
+//aula Adicionando Arquivos Js
+console.log('Bem vindo ao starter!!')
+
+//aula Comentários
+
+//comentário linha única
+/*
+Comentários
+multi
+linhas
+*/
+
+//3. Tipos de Dados--------------------------------
+
+//3.1. String
+
+console.log('Walison')
+console.log("Walison")
+console.log(`
+
+Walison
+
+${1+1}
+
+`) //multi line e permite adicionar valores (INTERPOLAÇÃO)
+
