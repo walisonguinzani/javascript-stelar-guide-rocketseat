cfbbcb3a00f66989dc986f9eeb468bf93a66921a
Walison Guinzani <w.guinzani@gmail.com>
2021-07-16 18:09:45 -0300

    return-values-into-a-function

--- functions/functions.js
+++ functions/functions.js
@@ -1,9 +1,17 @@
-//6. Agrupamentos e parametros
+//6.2 Retornando valores dentro da Funcao
 
+let total = 0
 //function expression or function anonymous
 //parametros (parameters)
 const sum = function(number1, number2){
-    console.log(number1+number2)
+    let total = number1+number2
+    return total
 }
 
-sum(2, 3) // arguments
+let number1 = 34
+let number2 = 25
+
+// console.log(`o numero 1 e ${number1}`)
+// console.log(`o numero 2 e ${number2}`)
+console.log(`a soma e ${sum(number1,number2)}`)
+console.log(total)
---
+++ functions/functions_2.js
@@ -0,0 +1,11 @@
+//6. Agrupamentos e parametros
+
+//function expression or function anonymous
+//parametros (parameters)
+const sum = function(number1, number2){
+    console.log(number1+number2)
+}
+
+sum(2, 3) // arguments
+
+
