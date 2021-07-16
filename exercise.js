708c945a0630ad9e5d98180915698f13c2817db0
Walison Guinzani <w.guinzani@gmail.com>
2021-07-15 23:05:22 -0300

    exercises-js

---
+++ exercise.js
@@ -0,0 +1,48 @@
+
+let weight
+
+console.log(typeof weight)
+
+let Name, age, stars, isSubscribed
+
+    Name: "Walison"
+    age: 26
+    stars: 10.5
+    isSubscribed: true
+
+
+let student = {
+    Name: "Walison",
+    age: 26,
+    stars: 10.5,
+    weight: 82.9,
+    isSubscribed: true
+
+}
+
+console.log(`${student.Name} de idade ${student.age} pesa ${student.weight}Kg.`)
+
+let Students = [
+]
+
+Students = [
+    student
+]
+
+
+console.log(Students)
+console.log(Students[0])
+
+const wali = {
+    Name: "Guinzani",
+    age: 28,
+    weight: 76.4,
+    isSubscribed: true
+}
+
+Students[1] = wali
+
+console.log(Students)
+
+console.log(a) // hoisting
+var a = 1
