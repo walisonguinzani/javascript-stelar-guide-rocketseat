07861c68aa0b5fe6b1a38ad7ebdd86cfd8335df1
Walison Guinzani <w.guinzani@gmail.com>
2021-07-17 15:28:02 -0300

    update-prototype

---
+++ manipulando_dados/script.js
@@ -0,0 +1,18 @@
+//7.2 
+
+/*
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    * __proto__
+
+*/
+
+console.log("walison".length)
+
+
+console.log(26.0.hasOwnProperty())
+
+console.log(["a", "b"].length)
+
