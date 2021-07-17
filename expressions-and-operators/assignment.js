4a595f9d9165182d77f6794f693c9fa878160ea7
Walison Guinzani <w.guinzani@gmail.com>
2021-07-17 20:33:02 -0300

    logic-operators

---
+++ expressions-and-operators/assignment.js
@@ -0,0 +1,24 @@
+//8.9 Operatores de atribuicao (Assignment)
+
+let x
+
+// assignment
+x = 1
+
+// addition assignment
+x += 2
+
+// subtraction assignment
+x -= 1
+
+// multiplication assignment
+x *= 2
+
+// division assignment
+x /= 2
+
+// remainder, exponetiation
+x %= 2
+x **= 2
+
+console.log(x)
