57a04578bb0b7c96ab172bf44233cc9d215105cd
Walison Guinzani <w.guinzani@gmail.com>
2021-07-17 16:15:36 -0300

    manipulando-arrays

--- manipulando_dados/script.js
+++ manipulando_dados/script.js
@@ -1,4 +1,23 @@
-//7.11. Strings to Arrays
-let word = "manipulacao"
+//7.12. Manipulando Arrays
 
-console.log(Array.from(word))
+let techs = ["html", "css", "js"]
+
+
+//adicionar um item no fim
+techs.push("nodejs")
+//add no comeco
+techs.unshift("sql")
+// remove from end
+//techs.pop()
+// remove from start
+//techs.shift()
+// take just some elements from array
+//console.log(techs.slice(1, 3))
+// remove one or more item from anywhere position of array
+//techs.splice(1, 1)
+// find the position of and element in array
+let index = techs.indexOf('html')
+techs.splice(index, 1)
+
+console.log(index)
+console.log(techs)
