09d2cbfef4fa786cbcc9d82b32e30ae28d37ef07
Walison Guinzani <w.guinzani@gmail.com>
2021-07-17 19:46:25 -0300

    operators

---
+++ expressions-and-operators/operators.js
@@ -0,0 +1,9 @@
+//8.1 Expressions and Operatos
+
+let number = 1
+
+console.log(number + 1) // binario soma de dois valores
+
+console.log(++number) //unario incremento em uma variavel
+
+console.log(true ? 'Hello' : 'world') // ternario possui tres expressores 
---
+++ expressions-and-operators/script.js
@@ -0,0 +1,9 @@
+//8.2 new
+
+let number = 1
+
+console.log(number + 1) // binario soma de dois valores
+
+console.log(++number) //unario incremento em uma variavel
+
+console.log(true ? 'Hello' : 'world') // ternario possui tres expressores 
