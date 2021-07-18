2875bbf9ad4467fb55b97d3b5c0d3d12427976ea
Walison Guinzani <w.guinzani@gmail.com>
2021-07-17 21:37:33 -0300

    update-for

---
+++ estruturas-de-repeticao/for.js
@@ -0,0 +1,12 @@
+// 10.1 For
+
+// break para execucao do loop
+// continue para continuar a repeticao do momento
+
+for (let i = 10; i > 0; i--){
+    if (i === 5){
+        continue;
+    }
+
+    console.log(i)
+}
