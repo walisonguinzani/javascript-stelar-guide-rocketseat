3b79e07fdb8ca9ae1192961bd18656bc21e53b3b
Walison Guinzani <w.guinzani@gmail.com>
2021-07-17 21:28:35 -0300

    throw-and-try-catch

--- condicionais-e-controle-de-fluxo/script.js
+++ condicionais-e-controle-de-fluxo/script.js
@@ -1,5 +1,21 @@
 //9.4 Throw e Try/Catch
 
-// throw
+// throw - disparar
+function sayMyName(name = '') {
+    if (name === ''){
+        throw new Error("Nome e necessario")
+    }
 
-//
+    console.log(name)
+}
+
+//try...catch - pegar
+
+try{
+    sayMyName('Walison')
+
+}catch(e){
+    console.log(e)
+}
+
+console.log('apos o try/catch')
